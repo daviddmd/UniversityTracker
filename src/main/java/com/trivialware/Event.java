66c7102245f397ecbfaef26f457048475f2f9aed
David Duarte <102606398+daviddmd@users.noreply.github.com>
2022-08-22 16:06:24 +0100

    Initial Commit

---
+++ src/main/java/com/trivialware/Event.java
@@ -0,0 +1,4 @@
+package com.trivialware;
+
+public class Event {
+}
---
+++ src/main/java/com/trivialware/Person.java
@@ -0,0 +1,9 @@
+package com.trivialware;
+
+public class Person {
+    enum Role {
+        STUDENT,
+        TEACHER,
+        WORKER
+    }
+}
